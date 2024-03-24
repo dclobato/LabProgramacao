43d614981e28848949a4aaebad4e5205c9e44704
Daniel Correa Lobato <daniel@lobato.org>
2024-03-23 22:45:21 -0300

    Changes -- Mudanca na forma de validação da senha

--- src/forms/auth.py
+++ src/forms/auth.py
@@ -1,5 +1,6 @@
 import re
 
+from flask import current_app
 from flask_wtf import FlaskForm
 from wtforms.fields.simple import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import Email, InputRequired, EqualTo, ValidationError, Length
@@ -9,19 +10,38 @@ from wtforms.validators import Email, InputRequired, EqualTo, ValidationError, L
 class ValidaSenha:
     def validate_password(self, password):
         # Definir a complexidade da senha na forma de expressões regulares
-        min_max_length = r"^(?=.{8,32}$)"  # 8 a 32 símbolos
+        expressoes = []
+        min = current_app.config.get("PASSWORD_MIN", 8)
+        max = current_app.config.get("PASSWORD_MAX", 64)
+        expressoes.append(f"^(?=.{{{min},{max}}}$)")
+        mensagem = f"A sua senha precisa ter entre {min} e {max} caracteres"
+
         upper = r"(?=.*[A-Z])"  # Pelo menos 1 letra maiúscula
         lower = r"(?=.*[a-z])"  # Pelo menos 1 letra minúscula
         number = r"(?=.*[0-9])"  # Pelo menos um número
         special = r"(?=.*[!@#\$%\&'\(\)*\+,\-./:;<=>?@\[\\\]^_`{|}~])"  # Pelo menos um caracter especial
 
-        # Combina as expressões regulares em uma única expressão
-        pattern = min_max_length + upper + lower + number + special
+        if current_app.config.get("PASSWORD_MAIUSCULA", False):
+            expressoes.append(upper)
+            mensagem = mensagem + ", letras maiúsculas"
+        if current_app.config.get("PASSWORD_MINUSCULA", False):
+            expressoes.append(lower)
+            mensagem = mensagem + ", letras minúsculas"
+        if current_app.config.get("PASSWORD_NUMERO", False):
+            expressoes.append(number)
+            mensagem = mensagem + ", números"
+        if current_app.config.get("PASSWORD_SIMBOLO", False):
+            expressoes.append(special)
+            mensagem = mensagem + ", símbolos especiais"
+
+        pattern = "".join(expressoes)
+
+        # Troca a última ocorrência de vírgula, se houver, por ' e '
+        pos = mensagem.rfind(', ')
+        if pos > -1: mensagem = mensagem[:pos] + ' e ' + mensagem[pos + 2:]
 
         if not re.match(pattern, str(password.data)):
-            raise ValidationError(
-                "A sua senha precisa ter entre 8 e 32 caracteres e conter letras maiúsculas, "
-                "minúsculas, números e símbolos especiais")
+            raise ValidationError(mensagem)
 
 
 class LoginForm(FlaskForm):
